ead4397e8d09a612295538aef0251bc127d5caae
Oliver Dressler <odressler@gmail.com>
2013-11-14 18:10:23 +0100

    New ZVV interface, Multiple Connection checker and off-line time-keeping.

--- engine.py
+++ engine.py
@@ -2,6 +2,36 @@ import json
 import urllib
 from dateutil.parser import parse
 from datetime import datetime
+import time
+from math import ceil
+
+###
+# The interfaces for the ZVV and SBB API
+###
+
+
+class ZVVTimeTable:
+    def __init__(self):
+        params = {'input': 'Zuerich, Regensbergbruecke', 'dirInput': 'Zuerich, Schwammendingerplatz'}
+
+        self.base_url = 'http://online.fahrplan.zvv.ch//bin/stboard.exe/dn?L=vs_widgets&{}&additionalTime=0&maxJourneys=1&start=yes&requestType=0'
+
+    def get_next_connection(self, station, direction):
+        '''direction has to be an end station.'''
+
+        # Fix up url
+        params = {'input': station, 'dirInput': direction}
+        url = self.base_url.format(urllib.urlencode(params))
+
+        # Get data and modify
+        timetableData = urllib.urlopen(url)
+        timetableDataString = timetableData.read().decode(encoding='UTF-8')
+        timetableDataStringTrunc = timetableDataString[14:]
+
+        data = json.loads(timetableDataStringTrunc)
+        journey = data[u'journey'][0]
+
+        return journey[u'pr'], int(journey[u'countdown_val'])
 
 
 class SBBTimeTable:
@@ -46,13 +76,61 @@ class SBBTimeTable:
         else:
             msg = '{}: Run it\'s LATE!'.format(line)
 
-        return msg, time_left
+        return line, minutes
+
+
+class MinuteCountdown:
+    '''A simple countdown lazy minute countdown'''
+    def __init__(self, minutes):
+        self.start_val = minutes
+        self.started = time.time()
+
+    def get_value(self):
+        minutes_passed = int(ceil((time.time() - self.started)/60))
+        val = self.start_val - minutes_passed
+        if val<0: val=0
+        return val
+
+
+class ConnectionTracker:
+    '''Can check connections to different targets'''
+
+    def __init__(self, api, station, targets):
+        self.api = api
+        self.station = station
+        self.targets = targets
+
+        # Set up countdown container
+        self.countdown = False
+        self.current_msg = False
+
+    def check_all(self):
+        # iterate over connections, find minimal time
+        min_con = (False, 1000)
+        for t in self.targets:
+            con = self.api.get_next_connection(self.station, t)
+            if con[1] < min_con[1]:
+                min_con = con
+
+        # Set up the Countdown
+        self.countdown = MinuteCountdown(min_con[1])
+        self.line = min_con[0]
+
+    def get_next(self):
+        if self.countdown:
+            return '{} in {} minutes'.format(self.line, self.countdown.get_value())
+        else:
+            return 'No Data Yet...'
 
 
 if __name__=='__main__':
     station = 'Zuerich, Regensbergbruecke'
-    target = 'Zuerich, ETH Hoenggerberg'
+    targets = ['Zuerich, Unteraffoltern', 'Zuerich, Muehlacker']
 
-    sbb = SBBTimeTable()
-    msg, seconds_left = sbb.get_next_connection(station, target)
-    print msg
+    api = ZVVTimeTable()
+    next_connection = ConnectionTracker(api, station, targets)
+
+    while True:
+        next_connection.check_all()
+        print next_connection.get_next()
+        time.sleep(20)
